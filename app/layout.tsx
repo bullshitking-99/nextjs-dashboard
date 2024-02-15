5440b0730c136288cf4c9158a4222066125a9d40
李名河 <2219845592@qq.com>
2024-02-15 15:40:43 +0800

    chapter 16

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,6 +1,17 @@
 import '@/app/ui/global.css';
 import { inter } from '@/app/ui/fonts';
 import clsx from 'clsx';
+import { Metadata } from 'next';
+
+// 定制化metadata，会自动被所有使用该layout的page继承
+export const metadata: Metadata = {
+  title: {
+    template: '%s | Acme Dashboard', // 前面带上该页面metadata的title
+    default: 'Acme Dashboard',
+  },
+  description: 'The official Next.js Learn Dashboard built with App Router.',
+  metadataBase: new URL('https://next-learn-dashboard.vercel.sh'),
+};
 
 export default function RootLayout({
   children,
